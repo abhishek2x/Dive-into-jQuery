dac8cd31abc17159c0b3333bbc71627334b31713
Abhishek Srivastava <53976003+abhishek2x@users.noreply.github.com>
2020-01-14 16:08:12 +0530

    Thrrd Commit

---
+++ Day-3(Selectors-2)/quer.js
@@ -0,0 +1,18 @@
+function fn1(){
+    $("div").fadeToggle();
+}
+function fn2(){
+    $("#firstdiv, #seconddiv").fadeToggle();
+}
+function fn3(){
+    $("p:even, #thirddiv").fadeToggle();
+}
+function fn4(){
+    $("li").fadeToggle();
+}
+function fn5(){
+    $(".mydivs:even").fadeToggle();
+}
+// ----------Uses---------
+// .mydivs:first
+// .mydivs:even
