66819c43843a08e1a6d9f1df9df50fa3312ae26c
Abhishek Srivastava <53976003+abhishek2x@users.noreply.github.com>
2020-01-16 15:41:17 +0530

    Forth Commit

---
+++ Day-4(Event)/quer.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    $("#1").click(function fn1(){
+        $("#firstdiv").fadeToggle();
+    });
+    $("#2").dblclick(function fn1(){
+        $("#firstdiv").fadeToggle();
+    });
+    $("#2").dblclick(function fn1(){
+        $("#firstdiv").fadeToggle();
+    });
+    $("#2").dblclick(function fn1(){
+        $("#firstdiv").fadeToggle();
+    });
+})
+
